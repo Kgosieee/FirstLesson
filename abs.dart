797363d947fa5f01e7b787dbe3e14799dcc4f2ca
Kgosieee <125345947+Kgosieee@users.noreply.github.com>
2023-03-01 11:49:33 +0200

    second comit

--- abs.dart
+++ abs.dart
@@ -1,23 +1,24 @@
 void main() {
+Set <String> classmates = {};
+  print("Empty List: $classmates");
+  
+  /*statement to add classmates */
+  print("Enter Classmates in a set");
+  classmates.add("Kgosietsile");
+  classmates.add("isabella");
+  classmates.add("Shatho");
+  
+  
 
-    // Creating Map using Constructors
-  var marks = new Map();
-  
-   // Inserting values into Map
-  marks [60] = 'Isabella';
-  marks [85] = 'Shatho';
-  marks [95] = 'Kgosietsile';
-  
-  
-   // Printing Its content after insertion
-  print("Marks of students are: $marks");
-  
-  print(marks[60]);
-  print(marks['Shatho']);
-   
+// Printing boolean value
+  print("The value of check is: $classmates");
   // Counting the length of the set
-  int l = marks.length;
-
-// Printing length
+  int l = classmates.length;
+  // Printing length
   print("Length of the set is: $l");
+  // Finding the element in the set
+  bool check = classmates.contains("Kgosietsile");
+// Printing boolean value
+  print("The value of check is: $check");
+
 }
