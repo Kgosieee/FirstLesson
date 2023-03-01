f0b71fc70803f019e7f02af35ec4eec2f14e118b
Kgosieee <125345947+Kgosieee@users.noreply.github.com>
2023-03-01 11:50:55 +0200

    third commit

--- abs.dart
+++ abs.dart
@@ -1,24 +1,61 @@
 void main() {
-Set <String> classmates = {};
-  print("Empty List: $classmates");
-  
-  /*statement to add classmates */
-  print("Enter Classmates in a set");
-  classmates.add("Kgosietsile");
-  classmates.add("isabella");
-  classmates.add("Shatho");
-  
-  
+// Declaring set with value
+  var gfg = <String>{'Hello Kgosietsile'};
 
-// Printing boolean value
-  print("The value of check is: $classmates");
-  // Counting the length of the set
-  int l = classmates.length;
-  // Printing length
+// Printing Set
+  print("Value in the set is: $gfg");
+
+// Adding an element in the set
+  gfg.add("Shatho Gabonewe");
+
+// Printing Set
+  print("Values in the set is: $gfg");
+
+// Adding multiple values to the set
+  var names = {"Kgosie", "Shatho", "Isabella"};
+  gfg.addAll(names);
+
+// Printing Set
+  print("Values in the set is: $gfg");
+
+// Getting element at Index 0
+  var geek = gfg.elementAt(0);
+
+// Printing the element
+  print("Element at index 0 is: $geek");
+
+// Counting the length of the set
+  int l = gfg.length;
+
+// Printing length
   print("Length of the set is: $l");
-  // Finding the element in the set
-  bool check = classmates.contains("Kgosietsile");
+
+// Finding the element in the set
+  bool check = gfg.contains("Kgosie");
+
 // Printing boolean value
   print("The value of check is: $check");
 
+// Removing an element from the set
+  gfg.remove("Hello Kgosietsile");
+
+// Printing Set
+  print("Values in the set is: $gfg");
+
+// Using forEach in set
+  print(" ");
+  print("Using forEach in set:");
+  gfg.forEach((element) {
+    if (element == "Kgosietsile") {
+      print("Found");
+    } else {
+      print("Not Found");
+    }
+  });
+
+// Deleting elements from set
+  gfg.clear();
+
+// Printing set
+  print("Values in the set is: $gfg");
 }
